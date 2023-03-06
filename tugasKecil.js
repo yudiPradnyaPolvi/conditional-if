19ab7e8ab2279ef9477aed7dfe883bd517698f50
yudiPradnyaPolvi <pradnyakemenuh@gmail.com>
2023-03-06 21:58:04 +0800

    first commit

---
+++ tugasKecil.js
@@ -0,0 +1,21 @@
+let cuaca = "cerah"
+let adaSepatuHitam = "tidak"
+let sepatuPutih="ada"
+
+if(cuaca =="cerah"){
+    console.log ("Sumbul membeli sepatu")
+
+}else{
+    console.log ("membeli online")
+}
+if (adaSepatuHitam=="tidak"){
+   console.log ("Sumbul tidak jadi membeli sepatu hitam maka ")
+    if (sepatuPutih=="ada"){
+        console.log("membeli sepatu putih dan kauh kaki putih")
+    }
+    else{
+        console.log("tidak jadi beli")
+    }
+
+}
+console.log ("jadi sumbul memmbeli Sepatu")
